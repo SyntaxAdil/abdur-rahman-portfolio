b29084f717a787714e034d47dd1df58ad6ac004d
SyntaxAdil <abdurrahmanadil005@gmail.com>
2026-05-08 16:46:12 +0600

    revalidath after 60 sec portfolio data

--- lib/porject.ts
+++ lib/porject.ts
@@ -3,6 +3,7 @@
 export const getProject = async () => {
   const res = await fetch(
     `https://app-portfolio-manager.vercel.app/api/project/${process.env.PM_USER_ID}`,
+    { next: { revalidate: 60 } },
   );
   const data = await res.json();
   return data.data;
